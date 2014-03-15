25f7f812a6e02b767b51070189cf2e901952f3e9
David Siroky <siroky@dasir.cz>
2014-03-15 11:56:07 +0100

    update

--- README.rst
+++ README.rst
@@ -1,1 +1,4 @@
 BeagleBone(Black) QEMU emulation
+
+https://wiki.linaro.org/Resources/HowTo/Qemu-beagleboard
+http://www.cnx-software.com/2011/09/26/beagleboard-emulator-in-ubuntu-with-qemu/
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo qemu-system-arm -M beagle -m 256 -sd ./beagle_sd.img -clock unix -serial stdio
+qemu-system-arm -M beagle -m 256 -sd ./beagle_sd.img -clock unix -serial stdio -device usb-kbd -device usb-mouse -usb -device usb-net,netdev=mynet -netdev user,id=mynet
